af43bb1fd8d9ce9c4243127012a1f6f0f87ae0d5
David Mayes <davidmayes@Davids-MBP.local>
2018-09-17 02:40:13 -0400

    added dynamic menu option

--- footer.php
+++ footer.php
@@ -25,6 +25,9 @@
           <div class="site-footer__col-three">
             <h3 class="headline headline--small">Learn</h3>
             <nav class="nav-list">
+            <!-- <?php wp_nav_menu(array(
+              'theme_location' => 'footerLocationOne'
+            )); ?> -->
               <ul>
                 <li><a href="#">Legal</a></li>
                 <li><a href="<?php echo site_url('/privacy-policy')?>">Privacy</a></li>
@@ -37,6 +40,9 @@
         <div class="site-footer__col-four">
           <h3 class="headline headline--small">Connect With Us</h3>
           <nav>
+          <!-- <?php wp_nav_menu(array(
+              'theme_location' => 'footerLocationTwo'
+            )); ?> -->
             <ul class="min-list social-icons-list group">
               <li><a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
               <li><a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
