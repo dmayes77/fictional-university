d25bb87f22f177407057bf773bd63059de981703
David Mayes <davidmayes@Davids-MBP.local>
2018-09-17 01:09:36 -0400

    added if statement

--- page.php
+++ page.php
@@ -16,10 +16,12 @@
 
   <div class="container container--narrow page-section">
 
-    <?php if (wp_get_post_parent_id(get_the_id())) { ?>
+    <?php 
+      $id = wp_get_post_parent_id(get_the_id());
+      if ($id) { ?>
       <div class="metabox metabox--position-up metabox--with-home-link">
-        <p><a class="metabox__blog-home-link" href="#">
-          <i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span></p>
+        <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($id); ?>">
+          <i class="fa fa-home" aria-hidden="true"></i> <?php echo get_the_title($id); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
       </div>
     <?php } ?>
     
