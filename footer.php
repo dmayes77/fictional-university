853e3aff3deb5dac79b7d8d9a003e6a1ca26ff09
David Mayes <davidmayes@Davids-MBP.local>
2018-09-17 02:43:18 -0400

    fixed social media icons

--- footer.php
+++ footer.php
@@ -44,11 +44,11 @@
               'theme_location' => 'footerLocationTwo'
             )); ?> -->
             <ul class="min-list social-icons-list group">
-              <li><a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-              <li><a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-              <li><a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
-              <li><a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-              <li><a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+              <li><a href="#" class="social-color-facebook"><i class="fab fa-facebook-f" aria-hidden="true"></i></a></li>
+              <li><a href="#" class="social-color-twitter"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
+              <li><a href="#" class="social-color-youtube"><i class="fab fa-youtube" aria-hidden="true"></i></a></li>
+              <li><a href="#" class="social-color-linkedin"><i class="fab fa-linkedin" aria-hidden="true"></i></a></li>
+              <li><a href="#" class="social-color-instagram"><i class="fab fa-instagram" aria-hidden="true"></i></a></li>
             </ul>
           </nav>
         </div>
