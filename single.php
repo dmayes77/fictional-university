752dfc11a16a7b40d21deb9a37631cd5de661aae
David Mayes <davidmayes@Davids-MBP.local>
2018-09-17 15:26:15 -0400

    moved date to top of post

--- single.php
+++ single.php
@@ -16,11 +16,15 @@
     </div>
 
     <div class="container container--narrow page-section">
-    <div class="metabox metabox--position-up metabox--with-home-link">
-        <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>">
-          <i class="fas fa-angle-double-left mr-2" aria-hidden="true"></i>Blog Home</a> 
-          <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('m.d.y'); ?> in <?php echo get_the_category_list(', ') ?></span></p>
+      <div class="metabox metabox--position-up metabox--with-home-link">
+          <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>">
+            <i class="fas fa-angle-double-left mr-2" aria-hidden="true"></i>Blog Home</a> 
+            <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> in <?php echo get_the_category_list(', ') ?></span>
+          </p>
       </div>
+      <br>
+      <small><?php the_time('F d, Y'); ?></small>
+      <hr>
       <div class="generic-content"><?php the_content(); ?></div>
     </div>
 
