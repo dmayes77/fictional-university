e1e751983757205a92e8f75a03fd4e57f33b52d9
David Mayes <davidmayes@Davids-MBP.local>
2018-09-17 02:39:28 -0400

    added dynamic menu option, meta tags, and dynamic body classes

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
   <head>
-    <meta charset="utf-8" />
+    <meta charset="<?php bloginfo('charset'); ?>" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
   </head>
-  <body>
+  <body <?php body_class(); ?> >
     
     <header class="site-header">
       <div class="container">
@@ -15,8 +15,11 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
+            <!-- <?php wp_nav_menu(array(
+              'theme_location' => 'headerMenuLocation'
+            )); ?> -->
             <ul>
-              <li><a href="<?php echo site_url('/about-us')?>">About Us</a></li>
+              <li <?php if (is_page('about-us') || wp_get_post_parent_id(0) == 15) echo 'class="current-menu-item"' ?> ><a href="<?php echo site_url('/about-us')?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
