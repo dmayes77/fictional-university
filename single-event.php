d211b1c4e4718b762808876a117f327643ae2416
David Mayes <davidmayes@Davids-MBP.local>
2018-09-20 17:03:24 -0400

    created relationship between events and programs

--- single-event.php
+++ single-event.php
@@ -33,9 +33,22 @@
       <br>
       <hr>
       <div class="generic-content"><?php the_content(); ?></div>
+      <?php 
+        $relatedPrograms = get_post_field('programs');
+        if ($relatedPrograms) {
+          echo '<hr class="section-break" >';
+          echo '<h2 class="headline headline--medium" >Related Program(s)</h2>';
+          echo '<ul class="link-list min-list">';
+          foreach($relatedPrograms as $program) { ?>
+            <li><a href="<?php echo get_the_permalink($program); ?>">
+              <?php echo get_the_title($program)?>
+            </a></li>
+          <?php } echo '</ul>'; 
+        } ?>
+        
     </div>
 
-  <?php };
+  <?php }
 
   get_footer();
 
