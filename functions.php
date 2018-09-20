f871c8df7039318e20ff895f7d8655bc732069c0
David Mayes <davidmayes@Davids-MBP.local>
2018-09-20 11:00:05 -0400

    added Programs

--- functions.php
+++ functions.php
@@ -20,7 +20,13 @@
   add_action('after_setup_theme', 'university_features');
 
   function university_adjust_queries($query) {
-    if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+    if (!is_admin() && is_post_type_archive('event') && $query->is_main_query()) {
+      $query->set('orderby', 'title');
+      $query->set('order', 'ASC');
+      $query->set('post_per_page', -1);
+    }
+    
+    if (!is_admin() && is_post_type_archive('event') && $query->is_main_query()) {
       $today = date('Ymd');
       $query->set('meta-key', 'event_date');
       $query->set('orderby', 'meta_value_num');
