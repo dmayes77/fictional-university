882bfb529e0689e07dfde60680ec8f6648b36046
David Mayes <dmayes77@gmail.com>
2018-09-29 11:49:25 -0400

    added registration and login logic

--- functions.php
+++ functions.php
@@ -99,4 +99,44 @@
 
   add_filter('acf/fields/google_map/api', 'universityMapKey');
   
+
+  //Redirect subscriber
+  function redirectSub() {
+    $currentUser = wp_get_current_user();
+    if (count($currentUser->roles) == 1 && $currentUser->roles[0] == 'subscriber') {
+      wp_redirect(site_url('/'));
+      exit;
+    }
+  }
+
+  add_action('admin_init', 'redirectSub');
+
+  function subAdminBar() {
+    $currentUser = wp_get_current_user();
+    if (count($currentUser->roles) == 1 && $currentUser->roles[0] == 'subscriber') {
+      show_admin_bar(false);
+    }
+  }
+
+  add_action('wp_loaded', 'subAdminBar');
+
+  //Login 
+  function headerUrl() {
+    return esc_url(site_url('/'));
+  }
+
+  add_filter('login_headerurl', 'headerUrl');
+
+  function loginCSS() {
+    wp_enqueue_style('university_main_styles', get_stylesheet_uri(), null, microtime());
+    wp_enqueue_style('Roboto-Condensed', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  }
+
+  add_action('login_enqueue_scripts', 'loginCSS');
+
+  function loginTitle() {
+    return get_bloginfo ('name');
+  }
+
+  add_filter('login_headertitle', 'loginTitle')
 ?>
